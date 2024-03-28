2875c08d049757b3b51458497c1c3bea8fe1cd1d
Dário Guimarães <dsgdevbraga@gmail.com>
2024-03-28 16:44:23 +0000

    formatting the code

--- app/src/main/java/org/trivialfitness/helloworld/HelloWorld.java
+++ app/src/main/java/org/trivialfitness/helloworld/HelloWorld.java
@@ -1,11 +1,13 @@
 package org.trivialfitness.helloworld;
 
 public class HelloWorld {
-  public static void helloWorldMessage() {
-    System.out.println("Hello World!");
-  }
 
-  public static void byeWorldMessage() {
-    System.out.println("Bye cruel World!");
-  }
+	public static void helloWorldMessage() {
+		System.out.println("Hello World!");
+	}
+
+	public static void byeWorldMessage() {
+		System.out.println("Bye cruel World!");
+	}
+
 }
