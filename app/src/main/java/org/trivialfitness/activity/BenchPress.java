eb32ae2a364ffa9f22dde6b1d1fc2e03db709e12
Darguima <dsgdevbraga@gmail.com>
2024-05-29 09:43:38 +0100

    Fix Hard Trains

--- app/src/main/java/org/trivialfitness/activity/BenchPress.java
+++ app/src/main/java/org/trivialfitness/activity/BenchPress.java
@@ -5,7 +5,7 @@ import org.trivialfitness.activity.activityType.WeightRepetitionsActivity;
 public class BenchPress extends WeightRepetitionsActivity {
 
 	public BenchPress(int repetitions, int weightKg) {
-		super("Bench Press", true, repetitions, 0.7, weightKg, 0.01);
+		super("Bench Press", false, repetitions, 0.7, weightKg, 0.01);
 	}
 
 	@Override
--- app/src/main/java/org/trivialfitness/activity/Weightlifting.java
+++ app/src/main/java/org/trivialfitness/activity/Weightlifting.java
@@ -5,7 +5,7 @@ import org.trivialfitness.activity.activityType.WeightRepetitionsActivity;
 public class Weightlifting extends WeightRepetitionsActivity {
 
 	public Weightlifting(int repetitions, int weightKg) {
-		super("Weightlifting", "Repetitions with Weight", true, repetitions, 5, weightKg, 2);
+		super("Weightlifting", "Repetitions with Weight", false, repetitions, 5, weightKg, 2);
 	}
 
 	@Override
